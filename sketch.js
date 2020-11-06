b1df766cee1903ac7e419eef8df1b44a20804938
sweathaprabhakar <70946320+sweathaprabhakar@users.noreply.github.com>
2020-11-06 20:03:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+const World = Matter.World;
+
+var engine,world;
+var ground;
+
+function setup() {
+ var canvas =  createCanvas(400,400);
+ engine = Engine.create();
+ world = engine.world;
+
+ var ground_options = {
+   isStatic: true
+ }
+ ground = Bodies.rectangle(200,390,200,20,ground_options);
+ World.add(world,ground);
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine); 
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20);
+  drawSprites();
+}
